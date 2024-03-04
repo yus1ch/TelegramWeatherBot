ff3bba4a0b694fe0d35005e69faaf87b8ed66e48
yus1c <ysk.n@ya.ru>
2024-03-04 12:08:39 +0300

    Initial commit

---
+++ weather.py
@@ -0,0 +1,58 @@
+import requests
+
+link = "https://api.open-meteo.com/v1/forecast?latitude={0}&longitude={1}&current=temperature_2m,weather_code,wind_speed_10m,wind_direction_10m&wind_speed_unit=ms"
+
+weather_codes = {
+        0: "Чистое небо",
+        1: "В основном ясно",
+        2: "Частично облачно",
+        3: "Пасмурно",
+        45: "Туман",
+        48: "Осаждение известкового тумана",
+        51: "Легкий дождик",
+        53: "Умеренный дождик",
+        55: "Интенсивный дождик",
+        56: "Легкий дождик со снегом",
+        57: "Сильный дождик со снегом",
+        61: "Легкий дождь",
+        63: "Умеренный дождь",
+        65: "Интенсивный дождь",
+        66: "Лёгкий замерзающий снег",
+        67: "Интенсивный замерзающий снег",
+        71: "Лёгкий снег",
+        73: "Умеренный снег",
+        75: "Интенсивный снег",
+        77: "Ледяные крупинки",
+        80: "Незначительный ливень",
+        81: "Умеренный ливень",
+        82: "Сильный ливень",
+        85:  "Незначительный снегопад",
+        86: "Сильный снегопад"
+    }
+wind_directions = ("С", "СВ", "В", "ЮВ", "Ю", "ЮЗ", "З", "СЗ")
+
+
+def current_weather(latitude, longitude):
+    """
+    Получение текущего прогноза погоды
+
+    Аргументы:
+    latitude - широта
+    longitude - долгота
+
+    Возвращает:
+    Температура воздуха(float), скорость ветра(float), направление ветра(str), тип погоды(str)
+
+    Оригинальная документация:
+    https://open-meteo.com/en/docs
+
+    """
+
+    r = requests.get(link.format(latitude, longitude)).json()["current"]
+
+    temperature = r["temperature_2m"]
+    wind_speed = r["wind_speed_10m"]
+    wind_direction = wind_directions[int((r["wind_direction_10m"] + 22.5) // 45 % 8)]
+    weather_code = weather_codes[r["weather_code"]]
+
+    return temperature, wind_speed, wind_direction, weather_code
